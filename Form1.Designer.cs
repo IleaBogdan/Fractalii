343d3bccb0e11f2016264abcda7170cd753912c1
Batman <139052441+IleaBogdan@users.noreply.github.com>
2024-09-11 21:17:40 +0300

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace Fractalii
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            pictureBox1 = new PictureBox();
+            textBox1 = new TextBox();
+            label1 = new Label();
+            label2 = new Label();
+            textBox2 = new TextBox();
+            button1 = new Button();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Location = new Point(-3, 47);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(802, 391);
+            pictureBox1.TabIndex = 1;
+            pictureBox1.TabStop = false;
+            pictureBox1.MouseDown += GainBox_MouseDown;
+            pictureBox1.MouseMove += GainBox_MouseMove;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(137, 12);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(125, 27);
+            textBox1.TabIndex = 2;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(54, 15);
+            label1.Name = "label1";
+            label1.Size = new Size(77, 20);
+            label1.TabIndex = 3;
+            label1.Text = "Angle left:";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(344, 15);
+            label2.Name = "label2";
+            label2.Size = new Size(86, 20);
+            label2.TabIndex = 5;
+            label2.Text = "Angle right:";
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(436, 12);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(125, 27);
+            textBox2.TabIndex = 4;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(622, 17);
+            button1.Name = "button1";
+            button1.Size = new Size(94, 29);
+            button1.TabIndex = 6;
+            button1.Text = "button1";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(button1);
+            Controls.Add(label2);
+            Controls.Add(textBox2);
+            Controls.Add(label1);
+            Controls.Add(textBox1);
+            Controls.Add(pictureBox1);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+        private PictureBox pictureBox1;
+        private TextBox textBox1;
+        private Label label1;
+        private Label label2;
+        private TextBox textBox2;
+        private Button button1;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,56 @@
+namespace Fractalii
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            pictureBox1.BackColor = Color.White;
+        }
+        // Variable that will hold the point from which to draw the next line
+        Point latestPoint;
+
+
+        private void GainBox_MouseDown(object sender, MouseEventArgs e)
+        {
+            using (Graphics g = pictureBox1.CreateGraphics())
+            {
+                // Draw next line and...
+                g.DrawLine(Pens.Red, new Point(10, 10), new Point(100, 200));
+            }
+
+            if ((e.Button & MouseButtons.Left) == MouseButtons.Left)
+            {
+                // Remember the location where the button was pressed
+                latestPoint = e.Location;
+            }
+        }
+
+        private void GainBox_MouseMove(object sender, MouseEventArgs e)
+        {
+            if ((e.Button & MouseButtons.Left) == MouseButtons.Left)
+            {
+                using (Graphics g = pictureBox1.CreateGraphics())
+                {
+                    // Draw next line and...
+                    g.DrawLine(Pens.Red, latestPoint, e.Location);
+
+                    // ... Remember the location
+                    latestPoint = e.Location;
+                }
+            }
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            using (Graphics g = pictureBox1.CreateGraphics())
+            {
+                // Draw next line and...
+                g.DrawLine(Pens.Red, new Point(10, 10), new Point(100, 200));
+            }
+        }
+    }
+}
