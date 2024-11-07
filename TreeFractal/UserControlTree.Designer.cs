b272ffeace0b809869bf742d216de504b3411c1f
Batman <139052441+IleaBogdan@users.noreply.github.com>
2024-11-07 21:15:45 +0200

    user control tree

---
+++ TreeFractal/UserControlTree.Designer.cs
@@ -0,0 +1,330 @@
+﻿namespace Fractalii.TreeFractal
+{
+    partial class UserControlTree
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            textBox_angle_left_tab1 = new TextBox();
+            textBox_angle_right_tab1 = new TextBox();
+            textBox_reduction_right_tab1 = new TextBox();
+            textBox_size_tab1 = new TextBox();
+            textBox_reduction_left_tab1 = new TextBox();
+            textBox_levels_tab1 = new TextBox();
+            textBox_width_tab1 = new TextBox();
+            finalColorSelect_treeFractal_tab1 = new Button();
+            initialColorSelect_treeFractal_tab1 = new Button();
+            label_angle_left_tab1 = new Label();
+            label_procent_reduction_right_tab1 = new Label();
+            label_angle_right_tab1 = new Label();
+            label_reduction_right_tab1 = new Label();
+            label_size_tab1 = new Label();
+            label_procent_reduction_left_tab1 = new Label();
+            button1 = new Button();
+            label_reduction_left_tab1 = new Label();
+            label_levels_tab1 = new Label();
+            label_width_max_tab1 = new Label();
+            label5 = new Label();
+            label_width_tab1 = new Label();
+            button2 = new Button();
+            SuspendLayout();
+            // 
+            // textBox_angle_left_tab1
+            // 
+            textBox_angle_left_tab1.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            textBox_angle_left_tab1.Location = new Point(70, 16);
+            textBox_angle_left_tab1.Margin = new Padding(3, 2, 3, 2);
+            textBox_angle_left_tab1.Name = "textBox_angle_left_tab1";
+            textBox_angle_left_tab1.Size = new Size(98, 23);
+            textBox_angle_left_tab1.TabIndex = 102;
+            // 
+            // textBox_angle_right_tab1
+            // 
+            textBox_angle_right_tab1.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            textBox_angle_right_tab1.Location = new Point(251, 16);
+            textBox_angle_right_tab1.Margin = new Padding(3, 2, 3, 2);
+            textBox_angle_right_tab1.Name = "textBox_angle_right_tab1";
+            textBox_angle_right_tab1.Size = new Size(98, 23);
+            textBox_angle_right_tab1.TabIndex = 103;
+            // 
+            // textBox_reduction_right_tab1
+            // 
+            textBox_reduction_right_tab1.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            textBox_reduction_right_tab1.Location = new Point(271, 46);
+            textBox_reduction_right_tab1.Name = "textBox_reduction_right_tab1";
+            textBox_reduction_right_tab1.Size = new Size(50, 23);
+            textBox_reduction_right_tab1.TabIndex = 107;
+            // 
+            // textBox_size_tab1
+            // 
+            textBox_size_tab1.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            textBox_size_tab1.Location = new Point(393, 16);
+            textBox_size_tab1.Margin = new Padding(3, 2, 3, 2);
+            textBox_size_tab1.Name = "textBox_size_tab1";
+            textBox_size_tab1.Size = new Size(97, 23);
+            textBox_size_tab1.TabIndex = 104;
+            // 
+            // textBox_reduction_left_tab1
+            // 
+            textBox_reduction_left_tab1.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            textBox_reduction_left_tab1.Location = new Point(90, 46);
+            textBox_reduction_left_tab1.Name = "textBox_reduction_left_tab1";
+            textBox_reduction_left_tab1.Size = new Size(55, 23);
+            textBox_reduction_left_tab1.TabIndex = 106;
+            // 
+            // textBox_levels_tab1
+            // 
+            textBox_levels_tab1.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            textBox_levels_tab1.Location = new Point(543, 16);
+            textBox_levels_tab1.Name = "textBox_levels_tab1";
+            textBox_levels_tab1.Size = new Size(42, 23);
+            textBox_levels_tab1.TabIndex = 105;
+            // 
+            // textBox_width_tab1
+            // 
+            textBox_width_tab1.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            textBox_width_tab1.Location = new Point(405, 49);
+            textBox_width_tab1.Name = "textBox_width_tab1";
+            textBox_width_tab1.Size = new Size(55, 23);
+            textBox_width_tab1.TabIndex = 108;
+            // 
+            // finalColorSelect_treeFractal_tab1
+            // 
+            finalColorSelect_treeFractal_tab1.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            finalColorSelect_treeFractal_tab1.Location = new Point(715, 14);
+            finalColorSelect_treeFractal_tab1.Margin = new Padding(3, 2, 3, 2);
+            finalColorSelect_treeFractal_tab1.Name = "finalColorSelect_treeFractal_tab1";
+            finalColorSelect_treeFractal_tab1.Size = new Size(86, 84);
+            finalColorSelect_treeFractal_tab1.TabIndex = 121;
+            finalColorSelect_treeFractal_tab1.Text = "Select final color";
+            finalColorSelect_treeFractal_tab1.UseVisualStyleBackColor = true;
+            // 
+            // initialColorSelect_treeFractal_tab1
+            // 
+            initialColorSelect_treeFractal_tab1.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            initialColorSelect_treeFractal_tab1.Location = new Point(623, 12);
+            initialColorSelect_treeFractal_tab1.Margin = new Padding(3, 2, 3, 2);
+            initialColorSelect_treeFractal_tab1.Name = "initialColorSelect_treeFractal_tab1";
+            initialColorSelect_treeFractal_tab1.Size = new Size(86, 88);
+            initialColorSelect_treeFractal_tab1.TabIndex = 120;
+            initialColorSelect_treeFractal_tab1.Text = "Select initial color";
+            initialColorSelect_treeFractal_tab1.UseVisualStyleBackColor = true;
+            // 
+            // label_angle_left_tab1
+            // 
+            label_angle_left_tab1.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            label_angle_left_tab1.AutoSize = true;
+            label_angle_left_tab1.Location = new Point(3, 19);
+            label_angle_left_tab1.Name = "label_angle_left_tab1";
+            label_angle_left_tab1.Size = new Size(61, 15);
+            label_angle_left_tab1.TabIndex = 112;
+            label_angle_left_tab1.Text = "Angle left:";
+            // 
+            // label_procent_reduction_right_tab1
+            // 
+            label_procent_reduction_right_tab1.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            label_procent_reduction_right_tab1.AutoSize = true;
+            label_procent_reduction_right_tab1.BackColor = SystemColors.ButtonHighlight;
+            label_procent_reduction_right_tab1.Location = new Point(324, 49);
+            label_procent_reduction_right_tab1.Name = "label_procent_reduction_right_tab1";
+            label_procent_reduction_right_tab1.Size = new Size(17, 15);
+            label_procent_reduction_right_tab1.TabIndex = 119;
+            label_procent_reduction_right_tab1.Text = "%";
+            // 
+            // label_angle_right_tab1
+            // 
+            label_angle_right_tab1.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            label_angle_right_tab1.AutoSize = true;
+            label_angle_right_tab1.Location = new Point(176, 19);
+            label_angle_right_tab1.Name = "label_angle_right_tab1";
+            label_angle_right_tab1.Size = new Size(69, 15);
+            label_angle_right_tab1.TabIndex = 114;
+            label_angle_right_tab1.Text = "Angle right:";
+            // 
+            // label_reduction_right_tab1
+            // 
+            label_reduction_right_tab1.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            label_reduction_right_tab1.AutoSize = true;
+            label_reduction_right_tab1.Location = new Point(176, 49);
+            label_reduction_right_tab1.Name = "label_reduction_right_tab1";
+            label_reduction_right_tab1.Size = new Size(89, 15);
+            label_reduction_right_tab1.TabIndex = 118;
+            label_reduction_right_tab1.Text = "Right Redution:";
+            // 
+            // label_size_tab1
+            // 
+            label_size_tab1.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            label_size_tab1.AutoSize = true;
+            label_size_tab1.Location = new Point(357, 19);
+            label_size_tab1.Name = "label_size_tab1";
+            label_size_tab1.Size = new Size(30, 15);
+            label_size_tab1.TabIndex = 115;
+            label_size_tab1.Text = "Size:";
+            // 
+            // label_procent_reduction_left_tab1
+            // 
+            label_procent_reduction_left_tab1.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            label_procent_reduction_left_tab1.AutoSize = true;
+            label_procent_reduction_left_tab1.BackColor = SystemColors.ButtonHighlight;
+            label_procent_reduction_left_tab1.Location = new Point(148, 49);
+            label_procent_reduction_left_tab1.Name = "label_procent_reduction_left_tab1";
+            label_procent_reduction_left_tab1.Size = new Size(17, 15);
+            label_procent_reduction_left_tab1.TabIndex = 117;
+            label_procent_reduction_left_tab1.Text = "%";
+            // 
+            // button1
+            // 
+            button1.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            button1.Location = new Point(819, 16);
+            button1.Margin = new Padding(3, 2, 3, 2);
+            button1.Name = "button1";
+            button1.Size = new Size(78, 73);
+            button1.TabIndex = 122;
+            button1.Text = "Generate recursively";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += genearte_treeFractal_recursive;
+            // 
+            // label_reduction_left_tab1
+            // 
+            label_reduction_left_tab1.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            label_reduction_left_tab1.AutoSize = true;
+            label_reduction_left_tab1.Location = new Point(3, 49);
+            label_reduction_left_tab1.Name = "label_reduction_left_tab1";
+            label_reduction_left_tab1.Size = new Size(81, 15);
+            label_reduction_left_tab1.TabIndex = 116;
+            label_reduction_left_tab1.Text = "Left Redution:";
+            // 
+            // label_levels_tab1
+            // 
+            label_levels_tab1.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            label_levels_tab1.AutoSize = true;
+            label_levels_tab1.Location = new Point(498, 19);
+            label_levels_tab1.Name = "label_levels_tab1";
+            label_levels_tab1.Size = new Size(39, 15);
+            label_levels_tab1.TabIndex = 113;
+            label_levels_tab1.Text = "Levels";
+            // 
+            // label_width_max_tab1
+            // 
+            label_width_max_tab1.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            label_width_max_tab1.AutoSize = true;
+            label_width_max_tab1.BackColor = SystemColors.ButtonHighlight;
+            label_width_max_tab1.Location = new Point(463, 52);
+            label_width_max_tab1.Name = "label_width_max_tab1";
+            label_width_max_tab1.Size = new Size(24, 15);
+            label_width_max_tab1.TabIndex = 111;
+            label_width_max_tab1.Text = "<7 ";
+            // 
+            // label5
+            // 
+            label5.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            label5.AutoSize = true;
+            label5.BackColor = SystemColors.ButtonHighlight;
+            label5.Location = new Point(587, 19);
+            label5.Name = "label5";
+            label5.Size = new Size(30, 15);
+            label5.TabIndex = 110;
+            label5.Text = "<15 ";
+            // 
+            // label_width_tab1
+            // 
+            label_width_tab1.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            label_width_tab1.AutoSize = true;
+            label_width_tab1.Location = new Point(357, 52);
+            label_width_tab1.Name = "label_width_tab1";
+            label_width_tab1.Size = new Size(42, 15);
+            label_width_tab1.TabIndex = 109;
+            label_width_tab1.Text = "Width:";
+            // 
+            // button2
+            // 
+            button2.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            button2.Location = new Point(904, 16);
+            button2.Name = "button2";
+            button2.Size = new Size(73, 73);
+            button2.TabIndex = 123;
+            button2.Text = "Generate iteratively";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += generate_treeFractal_iterative;
+            // 
+            // UserControl1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            Controls.Add(textBox_angle_left_tab1);
+            Controls.Add(textBox_angle_right_tab1);
+            Controls.Add(textBox_reduction_right_tab1);
+            Controls.Add(textBox_size_tab1);
+            Controls.Add(textBox_reduction_left_tab1);
+            Controls.Add(textBox_levels_tab1);
+            Controls.Add(textBox_width_tab1);
+            Controls.Add(finalColorSelect_treeFractal_tab1);
+            Controls.Add(initialColorSelect_treeFractal_tab1);
+            Controls.Add(label_angle_left_tab1);
+            Controls.Add(label_procent_reduction_right_tab1);
+            Controls.Add(label_angle_right_tab1);
+            Controls.Add(label_reduction_right_tab1);
+            Controls.Add(label_size_tab1);
+            Controls.Add(label_procent_reduction_left_tab1);
+            Controls.Add(button1);
+            Controls.Add(label_reduction_left_tab1);
+            Controls.Add(label_levels_tab1);
+            Controls.Add(label_width_max_tab1);
+            Controls.Add(label5);
+            Controls.Add(label_width_tab1);
+            Controls.Add(button2);
+            Name = "UserControl1";
+            Size = new Size(992, 106);
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private TextBox textBox_angle_left_tab1;
+        private TextBox textBox_angle_right_tab1;
+        private TextBox textBox_reduction_right_tab1;
+        private TextBox textBox_size_tab1;
+        private TextBox textBox_reduction_left_tab1;
+        private TextBox textBox_levels_tab1;
+        private TextBox textBox_width_tab1;
+        private Button finalColorSelect_treeFractal_tab1;
+        private Button initialColorSelect_treeFractal_tab1;
+        private Label label_angle_left_tab1;
+        private Label label_procent_reduction_right_tab1;
+        private Label label_angle_right_tab1;
+        private Label label_reduction_right_tab1;
+        private Label label_size_tab1;
+        private Label label_procent_reduction_left_tab1;
+        private Button button1;
+        private Label label_reduction_left_tab1;
+        private Label label_levels_tab1;
+        private Label label_width_max_tab1;
+        private Label label5;
+        private Label label_width_tab1;
+        private Button button2;
+        
+    }
+}
