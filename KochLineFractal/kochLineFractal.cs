cbb640cb0fb04e8de18f0d3cbcd1ddc5611c3600
Batman <139052441+IleaBogdan@users.noreply.github.com>
2024-11-08 20:09:31 +0200

    function (I am still thinking of how to make the fractal)

--- KochLineFractal/kochLineFractal.cs
+++ KochLineFractal/kochLineFractal.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.DirectoryServices.ActiveDirectory;
 using System.Linq;
 using System.Reflection;
 using System.Text;
@@ -9,12 +10,18 @@ namespace Fractalii.KochLineFractal
 {
     internal class kochLineFractal
     {
-        private void draw(Point start_point, Point end_point, PictureBox pb)
+        private static void draw(Point start_point, Point end_point, PictureBox pb)
         {
             using (Graphics g = pb.CreateGraphics())
             {
                 g.DrawLine(new Pen(Color.Red, 3), start_point, end_point);
             }
+        }
+        public static void generate_iterative(
+                Point ipoint, Point epoint, int levels
+            )
+        {
+            
         }
     }
 }
