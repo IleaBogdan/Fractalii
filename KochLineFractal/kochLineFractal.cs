017365bece986e5a8bbf59a638d3b12c12ea044e
Batman <139052441+IleaBogdan@users.noreply.github.com>
2024-11-10 23:20:32 +0200

    stupid bulllshit that I will fix tomorrow

--- KochLineFractal/kochLineFractal.cs
+++ KochLineFractal/kochLineFractal.cs
@@ -11,10 +11,24 @@ namespace Fractalii.KochLineFractal
 {
     internal class kochLineFractal
     {
+        public static double distance(Point end, Point begin)
+        {
+            double t1 = (end.X - begin.X), t2 = (end.Y - begin.Y);
+            return Math.Sqrt(t1*t1+t2*t2);
+        }
+        public static Point nextPoint(Point begin, double size)
+        {
+            double ang=Math.PI/3;
+            return new Point(begin.X+(int)(size*Math.Cos(ang)), 
+                begin.Y - (int)(size * Math.Sin(ang)));
+        }
         public static Point nextPoint(Point begin, Point end)
         {
-            Point next=new Point(100, 100);
-            return next;
+            Point mid=new Point((end.X+begin.X)/2, (end.Y+begin.Y)/2);
+            double dist = distance(end, begin);
+            // this bullshit only works if end.X==begin.X
+            mid.Y -= (int)(dist * (1.73205081)/2.0);
+            return mid;
         }
         private static Pen pen = new Pen(Color.Red, 3);
         public static void generate_iterative(
@@ -25,15 +39,22 @@ namespace Fractalii.KochLineFractal
             )
         {
             Queue<KochItem>q = new Queue<KochItem>();
+            Draw.draw_line(pb, ipoint, epoint, pen);
+            ipoint.X = epoint.X / 3;
+            epoint.X = 2*(epoint.X/3);
             KochItem k = new KochItem(ipoint, epoint, 0, width);
-            Draw.draw_line(pb, k.begin_point, k.end_point, pen);
-            
-            
-            // this part is still not done but it will be soon
-            Point MidPoint = nextPoint(k.begin_point, k.end_point);
-            q.Enqueue(KochItem.merge(new KochItem(k), MidPoint));
-            q.Enqueue(KochItem.merge(new KochItem(k), MidPoint, false));
 
+
+            // this part is still not done but it will be soon
+            //Point MidPoint = nextPoint(k.begin_point, (double)(k.end_point.X-k.begin_point.X));
+            // is io prost de aia nu mergea aia de mai sus da oricum o schimb
+            Point MidPoint = nextPoint(k.begin_point, k.end_point);
+
+
+            k.level = 1;
+            q.Enqueue(KochItem.merge(new KochItem(k), MidPoint, false));
+            q.Enqueue(KochItem.merge(new KochItem(k), MidPoint));
+            //System.Windows.Forms.MessageBox.Show(string.Format("{0}", [ MidPoint.X, MidPoint.Y]));
             while (q.Count > 0)
             {
                 k = q.Dequeue();
