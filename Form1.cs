15ea50e3d95bf42a290f9d6ffe7b87ca6c742657
Batman <139052441+IleaBogdan@users.noreply.github.com>
2025-05-01 17:47:06 +0300

    selected color displayed in a little box

--- Form1.cs
+++ Form1.cs
@@ -32,17 +32,38 @@ namespace Fractalii
         {
             pictureBox1.BackColor = bgC;
             userControl11.set_pictureBox(pictureBox1);
+            add_color(initialColor, initialColorSelect, "Select initial color");
+            add_color(finalColor, finalColorSelect, "Select final color");
         }
         // color setting
         public static Color initialColor = Color.Blue, finalColor = Color.Green;
+        
+
+        private static void add_color(Color color, Button b1, String text)
+        {
+            Bitmap bmp = new Bitmap(16, 16);
+            using (Graphics g = Graphics.FromImage(bmp))
+            {
+                g.FillRectangle(new SolidBrush(color), 0, 0, 16, 16);
+                g.DrawRectangle(Pens.Black, 0, 0, 15, 15);
+            }
+
+            b1.Image = bmp;
+            b1.TextImageRelation = TextImageRelation.TextAboveImage;
+            b1.TextAlign = ContentAlignment.MiddleCenter;
+            b1.ImageAlign = ContentAlignment.MiddleCenter;
+            b1.Text = text;
+            b1.Tag = initialColor;
+        }
         private void initialColorSelect_Click(object sender, EventArgs e)
         {
             Color_Selector colorSelector = new Color_Selector();
             DialogResult result = colorSelector.ShowDialog();
-
+            
             if (result == DialogResult.OK)
             {
                 initialColor = colorSelector.setColor;
+                add_color(initialColor, initialColorSelect, "Select initial color");
             }
         }
         private void finalColorSelect_Click(object sender, EventArgs e)
@@ -53,6 +74,7 @@ namespace Fractalii
             if (result == DialogResult.OK)
             {
                 finalColor = colorSelector.setColor;
+                add_color(finalColor, finalColorSelect, "Select final color");
             }
         }
 
