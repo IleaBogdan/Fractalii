e42f7a35c070c81a4687546e869056da99cda7f4
Batman <139052441+IleaBogdan@users.noreply.github.com>
2025-05-04 02:39:23 +0300

    ok we have colors, but it redraws all

--- DragonCurve/dragoncurveFractal.cs
+++ DragonCurve/dragoncurveFractal.cs
@@ -18,14 +18,36 @@ namespace Fractalii.DragonCurve
             );
         }
 
+        private static Color initialColor, finalColor;
+
+        private static int[] RGB = new int[3];
+        private static double[] RGBDif = new double[3];
+        private static void preDraw(int level, int maxLevel, double width)
+        {
+            pen.Width = (float)(width);
+
+            float fraction = (float)level / (float)maxLevel;
+            int green = RGB[1] + (int)(RGBDif[1] * fraction), red = RGB[0] + (int)(RGBDif[0] * fraction), blue = RGB[2] + (int)(RGBDif[2] * fraction);
+            pen.Color = Color.FromArgb(red >= 0 && red <= 255 ? red : 0, green >= 0 && green <= 255 ? green : 0, blue >= 0 && blue <= 255 ? blue : 0);
+        }
         public static void Generate(PictureBox pb, int iterations,
                                   double width, int size, Point startPoint)
         {
             pen.Width = (float)width;
+            initialColor = HomePage.initialColor;
+            finalColor = HomePage.finalColor;
+            pen.Color = initialColor;
+            RGB[0] = pen.Color.R;
+            RGB[1] = pen.Color.G;
+            RGB[2] = pen.Color.B;
+            RGBDif[0] = finalColor.R - RGB[0];
+            RGBDif[1] = finalColor.G - RGB[1];
+            RGBDif[2] = finalColor.B - RGB[2];
 
-            List<string> sequences = new List<string>();
+
+            List<string> sequences = new();
             string sequence = "F";
-            sequences.Add(sequence); // Add initial state
+            sequences.Add(sequence); 
 
             for (int i = 0; i < iterations; i++)
             {
@@ -33,11 +55,12 @@ namespace Fractalii.DragonCurve
                 sequences.Add(sequence);
             }
 
-            // Draw each sequence separately
+            int level = 0;
             foreach (var seq in sequences)
             {
+                preDraw(level++, iterations, width);
                 DrawSequence(pb, seq, startPoint, size);
-                Thread.Sleep(500); 
+                Thread.Sleep(500);
             }
         }
 
